087ff10ecea61146faeb84612b892ec95372dba1
ricar <ricar@RICARDOPIM-HP>
2025-03-26 21:43:12 -0300

    Adicionar arquivos de projeto.

---
+++ Money.Core/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Core.Models
+{
+    public class Categoria
+    {
+        public long Codigo { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public string CodigoUsuario { get; set; } = string.Empty;
+    }
+}
---
+++ Money.Core/Models/Transacao.cs
@@ -0,0 +1,22 @@
+﻿using Money.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Core.Models
+{
+    public class Transacao
+    {
+        public int Codigo { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+        public DateTime? DataTransacao { get; set; }
+        public ETipoTransacao Tipo { get; set; } = ETipoTransacao.Saida;
+        public decimal Valor { get; set; }
+        public long CodigoCategoria { get; set; }
+        public Categoria Categoria { get; set; } = null!;
+        public string CodigoUsuario { get; set; } = string.Empty;
+    }
+}
