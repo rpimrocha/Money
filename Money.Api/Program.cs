3a1848563d22b4a546fa06e12db9245372702a91
Ricardo Pim da Rocha <ricardopim@msn.com>
2025-08-14 23:12:58 -0300

    Adicionar endpoints de logout e roles no Program.cs
    Implementados novos endpoints para logout e obtenção de papéis do usuário autenticado. Adicionadas importações necessárias para manipulação de identidade e reivindicações. Ambos os endpoints requerem autorização para acesso.

--- Money.Api/Program.cs
+++ Money.Api/Program.cs
@@ -5,6 +5,7 @@ using Money.Api.Endpoints;
 using Money.Api.Handlers;
 using Money.Api.Models;
 using Money.Core.Handlers;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -47,5 +48,37 @@ app.MapEndponts();
 app.MapGroup("v1/identity")
     .WithTags("Identity")
     .MapIdentityApi<User>();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapPost("/logout", async (SignInManager<User> signInManager) =>
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+    })
+    .RequireAuthorization();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapGet("/roles", (ClaimsPrincipal user) =>
+    {
+        if (user.Identity is null || !user.Identity.IsAuthenticated)
+           return Results.Unauthorized();
+
+        var identity = (ClaimsIdentity)user.Identity;
+        var roles = identity
+            .FindAll(identity.RoleClaimType)
+            .Select(c => new
+            {
+                c.Issuer,
+                c.OriginalIssuer,
+                c.Type,
+                c.Value,
+                c.ValueType
+            });
+
+        return TypedResults.Json(roles);
+    })
+    .RequireAuthorization();
 
 app.Run();
