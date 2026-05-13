0667a50ed20965e74febbf05b97332d12036e17d
Ricardo Pim da Rocha <ricardopim@msn.com>
2026-05-12 23:58:31 -0300

    Adicionado ao front-end opção de aplicar tema escuro e claro.

---
+++ Money.Web/Configuracao.cs
@@ -0,0 +1,37 @@
+﻿using MudBlazor;
+
+namespace Money.Web
+{
+    public static class Configuracao
+    {
+        public static MudTheme Tema = new()
+        {
+            Typography = new Typography()
+            {
+                Default = new Default()
+                {
+                    FontFamily = ["Raleway", "sans-serif"],
+                }
+            },
+            Palette = new PaletteLight()
+            {
+                Primary = "#1EFA2D",
+                Secondary = Colors.LightGreen.Darken3,
+                Background = Colors.Green.Lighten4,
+                AppbarBackground = "#1EFA2D",
+                AppbarText = Colors.Shades.Black,
+                TextPrimary = Colors.Shades.Black,
+                PrimaryContrastText = Colors.Shades.Black,
+                DrawerText = Colors.Shades.Black,
+                DrawerBackground = Colors.Green.Lighten4,
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = Colors.LightGreen.Darken3,
+                Secondary = Colors.LightGreen.Darken3,
+                AppbarBackground = Colors.LightGreen.Accent3,
+                AppbarText = Colors.Shades.Black,
+            }
+        };
+    }
+}
